61b9e1a7a9a00fd70ba294bdaec62b861d4fdbfd
wynterwolf <shinyguns@gmail.com>
2020-04-22 12:42:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
-end
+
+    # Create a POST method in your controller (app.rb) to receive your form's params
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do 
+
+        ring = PigLatinizer.new
+        @string = ring.pig(params[:user_phrase])  
+        erb :piglatinize
+    end
+
+    end
